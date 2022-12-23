f9a8523516b6b393f5c4706f5e8d63ef096e1b6d
hwlee <bless2k@gmail.com>
2022-12-23 10:10:17 +0900

    docker file and jenkins pipeline config added

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -51,7 +51,7 @@ node {
     }
 
     stage('Push Docker Image') {
-        docker.withRegistry('localhost:5000') {
+        docker.withRegistry('minikube:5000') {
             app.push("${env.BUILD_NUMBER}")
             app.push("latest")
         }
