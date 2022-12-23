78b920a5be5a5ce3f868f4839b242d4881a3c106
hwlee <bless2k@gmail.com>
2022-12-23 23:22:44 +0900

    jenkins pipeline config modified.

--- src/main/java/com/bs/bsboard/AppMain.java
+++ src/main/java/com/bs/bsboard/AppMain.java
@@ -1,10 +1,14 @@
 package com.bs.bsboard;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AppMain {
+    private final static Logger log = LoggerFactory.getLogger(AppMain.class);
+
     public static void main(String[] args) {
         SpringApplication.run(AppMain.class, args);
     }
