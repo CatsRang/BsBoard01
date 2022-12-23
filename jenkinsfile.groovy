14d778e7b67ece60bf24a2ec5542f94e8280cc4f
hwlee <bless2k@gmail.com>
2022-12-23 16:33:07 +0900

    jenkins pipeline config modified.

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -51,7 +51,7 @@ node {
     }
 
     stage('Push Docker Image') {
-        docker.withRegistry('http://127.0.0.1:5000') {
+        docker.withRegistry('http://minikube:5000') {
             app.push("${env.BUILD_NUMBER}")
         }
     }
