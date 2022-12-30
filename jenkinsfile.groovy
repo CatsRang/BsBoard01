a555eaa63d68fd5f577400f48929c80bc4ccc807
hwlee <bless2k@gmail.com>
2022-12-31 04:05:08 +0900

    jenkins pipeline config modified.

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,14 +1,10 @@
 node {
-    properties(
-            [
-                    [$class: 'ParametersDefinitionProperty', parameterDefinitions:
-                            [
-                                    [$class: 'ChoiceParameterDefinition', description: 'Maven Active Profile', choices: ['dev', 'prd'], name: 'activeProfile'],
-                                    [$class: 'StringParameterDefinition', defaultValue: 'https://registry.hub.docker.com', description: 'Registry Url. ex) http://phis.harbor.io  https://registry.hub.docker.com', name: "dockerRegistry"],
-                                    [$class: 'StringParameterDefinition', defaultValue: 'dockerhub-bless2k', description: 'Registry Credential', name: "registryCredential"],
-                                    [$class: 'StringParameterDefinition', defaultValue: 'bless2k/pqm-api', description: 'Docker Image Name', name: "dockerImageName"]
-                            ]
-                    ]])
+    properties([[$class: 'ParametersDefinitionProperty', parameterDefinitions: [
+            [$class: 'ChoiceParameterDefinition', description: 'Maven Active Profile', choices: ['dev', 'prd'], name: 'activeProfile'],
+            [$class: 'StringParameterDefinition', defaultValue: 'https://registry.hub.docker.com', description: 'Registry Url. ex) http://phis.harbor.io  https://registry.hub.docker.com', name: "dockerRegistry"],
+            [$class: 'StringParameterDefinition', defaultValue: 'dockerhub-bless2k', description: 'Registry Credential', name: "registryCredential"],
+            [$class: 'StringParameterDefinition', defaultValue: 'bless2k/pqm-api', description: 'Docker Image Name', name: "dockerImageName"]
+    ]]])
 
     def app
 
