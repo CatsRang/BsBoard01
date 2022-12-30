b2f524b67f89ce9a27f57b8b157f39a1bf28e23f
hwlee <bless2k@gmail.com>
2022-12-30 21:07:59 +0900

    jenkins pipeline config modified.

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -42,15 +42,9 @@ node {
         }
     }
 
-    /*
     stage('Kubernetes Deploy') {
         withKubeConfig([credentialsId: 'kube_config']) {
-            sh 'curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"'
-            sh 'chmod u+x ./kubectl'
-//            sh "./kubectl delete deployment bsboard-b01 -n pqmtest"
-            sh "./kubectl apply -f k8s_deployment.yaml"
-            //kubernetesDeploy(configs: "k8s_deployment.yml", kubeconfigId: "kubernetes")
+            kubernetesDeploy(configs: "k8s_deployment.yml", kubeconfigId: "kubernetes")
         }
     }
-     */
 }
