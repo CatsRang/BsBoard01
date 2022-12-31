49e794710dce9319a1bccf06909b192df6ba3bda
hwlee <bless2k@gmail.com>
2022-12-31 16:10:12 +0900

    jenkins pipeline config modified.

--- jenkinsfile02.groovy
+++ jenkinsfile02.groovy
@@ -1,4 +1,4 @@
-node {
+pipeline {
     properties([[$class: 'ParametersDefinitionProperty', parameterDefinitions: [
             [$class: 'ChoiceParameterDefinition', description: 'Maven Active Profile', choices: ['dev', 'prd'], name: 'activeProfile'],
             [$class: 'StringParameterDefinition', defaultValue: 'https://registry.hub.docker.com', description: 'Registry Url. ex) http://phis.harbor.io  https://registry.hub.docker.com', name: "dockerRegistry"],
@@ -12,31 +12,35 @@ node {
         }
     }
 
-    def app
-
-    stage('Preparation') { // for display purposes
-        echo "Current workspace : ${workspace}"
-    }
-
-    stage('Checkout') {
-        checkout scm
-    }
-
-    stage('Build Package') {
-        withMaven(
-                maven: 'MavenM3'
-        ) {
-            sh "mvn -P ${activeProfile} -Dmaven.test.skip=true clean package"
+    stages {
+        stage('Preparation') { // for display purposes
+            echo "Current workspace : ${workspace}"
         }
-    }
 
-    stage('Archive') {
-        archiveArtifacts artifacts: '**/target/*.jar'
-    }
+        stage('Checkout') {
+            checkout scm
+        }
 
-    stage('Build Docker Image') {
-        container("container-test01") {
-            sh "echo Hello from $POD_CONTAINER"
+        /*
+        stage('Build Package') {
+            withMaven(
+                    maven: 'MavenM3'
+            ) {
+                sh "mvn -P ${activeProfile} -Dmaven.test.skip=true clean package"
+            }
+        }
+         */
+
+        /*
+        stage('Archive') {
+            archiveArtifacts artifacts: '**/target/*.jar'
+        }
+        */
+
+        stage('Build Docker Image') {
+            container("container-test01") {
+                sh "echo Hello from $POD_CONTAINER"
+            }
         }
     }
 }
