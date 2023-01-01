0a4e95a38a59b66cb294b40f93b779699c8b3a5d
hwlee <bless2k@gmail.com>
2023-01-01 17:15:04 +0900

    Revert "jenkins pipeline config modified."
    This reverts commit 4584fe80

--- jenkinsfile02.groovy
+++ jenkinsfile02.groovy
@@ -6,52 +6,61 @@ properties([parameters([
 ])])
 
 pipeline {
-    agent { node { label "pod-kaniko" } }
+    agent none
 
     options {
         skipDefaultCheckout(true)
     }
 
     stages {
-        stage('Checkout') {
-            steps {
-                echo "> Current workspace : ${workspace}"
-                echo "> activeProfile : ${activeProfile}"
-                echo "> dockerRegistry : ${dockerRegistry}"
-                echo "> registryCredential : ${registryCredential}"
-                echo "> dockerImageName : ${dockerImageName}"
+        stage('Build') {
+            agent { node { label "pod-kaniko" } }
 
-                checkout scm
-            }
-        }
-        stage('Build Package') {
-            steps {
-                container(name: "container-maven") {
-                    sh "mvn -P ${activeProfile} -Dmaven.test.skip=true clean package"
+            stages {
+                stage('Checkout') {
+                    steps {
+                        echo "> Current workspace : ${workspace}"
+                        echo "> activeProfile : ${activeProfile}"
+                        echo "> dockerRegistry : ${dockerRegistry}"
+                        echo "> registryCredential : ${registryCredential}"
+                        echo "> dockerImageName : ${dockerImageName}"
+
+                        checkout scm
+                    }
                 }
-            }
-        }
+                stage('Build Package') {
+                    agent { node { label "pod-kaniko" } }
 
-        stage('Build Docker Image') {
-            steps {
-                container(name: "container-kaniko", shell: "/busybox/sh") {
-                    withCredentials([file(credentialsId: 'secret-kaniko', variable: 'CONF_KANIKO')]) {
-                        sh "mkdir -p /kaniko/.docker"
-                        sh "cp $CONF_KANIKO /kaniko/.docker/config.json"
-                        sh "/kaniko/executor -f `pwd`/Dockerfile --context=`pwd` --insecure --skip-tls-verify --cache=true --destination=$dockerRegistry/$dockerImageName:$env.BUILD_NUMBER"
+                    steps {
+                        container(name: "container-maven") {
+                            sh "mvn -P ${activeProfile} -Dmaven.test.skip=true clean package"
+                        }
+                    }
+                }
+
+                stage('Build Docker Image') {
+                    agent { node { label "pod-kaniko" } }
+
+                    steps {
+                        container(name: "container-kaniko", shell: "/busybox/sh") {
+                            withCredentials([file(credentialsId: 'secret-kaniko', variable: 'CONF_KANIKO')]) {
+                                sh "mkdir -p /kaniko/.docker"
+                                sh "cp $CONF_KANIKO /kaniko/.docker/config.json"
+                                sh "/kaniko/executor -f `pwd`/Dockerfile --context=`pwd` --insecure --skip-tls-verify --cache=true --destination=$dockerRegistry/$dockerImageName:$env.BUILD_NUMBER"
+                            }
+                        }
                     }
                 }
             }
         }
 
         stage('Kubernetes Deploy') {
+            agent none
+
             steps {
                 withKubeConfig([credentialsId: 'kube-secret']) {
                     sh "sed -i \"s,__IMAGE_NAME__,$dockerRegistry/$dockerImageName:$env.BUILD_NUMBER,\" k8s_deployment.yaml"
-
-                    sh 'curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"'
-                    sh 'chmod u+x ./kubectl'
-                    sh "./kubectl apply -f k8s_deployment.yaml"
+                    sh "/usr/bin/kubectl apply -f k8s_deployment.yaml"
                 }
             }
         }
