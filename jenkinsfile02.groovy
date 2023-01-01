0d1b7b64a60f537cd92e1156a5f0aa4c7b161d11
hwlee <bless2k@gmail.com>
2023-01-01 16:26:06 +0900

    jenkins pipeline config modified.

--- jenkinsfile02.groovy
+++ jenkinsfile02.groovy
@@ -13,39 +13,42 @@ pipeline {
     }
 
     stages {
-        stage('Checkout') {
+        stage('Build') {
             agent { node { label "pod-kaniko" } }
 
-            steps {
-                echo "> Current workspace : ${workspace}"
-                echo "> activeProfile : ${activeProfile}"
-                echo "> dockerRegistry : ${dockerRegistry}"
-                echo "> registryCredential : ${registryCredential}"
-                echo "> dockerImageName : ${dockerImageName}"
+            stages {
+                stage('Checkout') {
+                    steps {
+                        echo "> Current workspace : ${workspace}"
+                        echo "> activeProfile : ${activeProfile}"
+                        echo "> dockerRegistry : ${dockerRegistry}"
+                        echo "> registryCredential : ${registryCredential}"
+                        echo "> dockerImageName : ${dockerImageName}"
 
-                checkout scm
-            }
-        }
-
-        stage('Build Package') {
-            agent { node { label "pod-kaniko" } }
-
-            steps {
-                container(name: "container-maven") {
-                    sh "mvn -P ${activeProfile} -Dmaven.test.skip=true clean package"
+                        checkout scm
+                    }
                 }
-            }
-        }
+                stage('Build Package') {
+                    agent { node { label "pod-kaniko" } }
 
-        stage('Build Docker Image') {
-            agent { node { label "pod-kaniko" } }
+                    steps {
+                        container(name: "container-maven") {
+                            sh "mvn -P ${activeProfile} -Dmaven.test.skip=true clean package"
+                        }
+                    }
+                }
 
-            steps {
-                container(name: "container-kaniko", shell: "/busybox/sh") {
-                    withCredentials([file(credentialsId: 'secret-kaniko', variable: 'CONF_KANIKO')]) {
-                        sh "mkdir -p /kaniko/.docker"
-                        sh "cp $CONF_KANIKO /kaniko/.docker/config.json"
-                        sh "/kaniko/executor -f `pwd`/Dockerfile --context=`pwd` --insecure --skip-tls-verify --cache=true --destination=$dockerRegistry/$dockerImageName:$env.BUILD_NUMBER"
+                stage('Build Docker Image') {
+                    agent { node { label "pod-kaniko" } }
+
+                    steps {
+                        container(name: "container-kaniko", shell: "/busybox/sh") {
+                            withCredentials([file(credentialsId: 'secret-kaniko', variable: 'CONF_KANIKO')]) {
+                                sh "mkdir -p /kaniko/.docker"
+                                sh "cp $CONF_KANIKO /kaniko/.docker/config.json"
+                                sh "/kaniko/executor -f `pwd`/Dockerfile --context=`pwd` --insecure --skip-tls-verify --cache=true --destination=$dockerRegistry/$dockerImageName:$env.BUILD_NUMBER"
+                            }
+                        }
                     }
                 }
             }
