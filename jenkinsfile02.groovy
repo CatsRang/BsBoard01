d2f75d8034563e87d8c544b16a450dd70b5c17f4
hwlee <bless2k@gmail.com>
2023-01-01 16:44:26 +0900

    jenkins pipeline config modified.

--- jenkinsfile02.groovy
+++ jenkinsfile02.groovy
@@ -6,58 +6,51 @@ properties([parameters([
 ])])
 
 pipeline {
-    agent none
+    agent { node { label "pod-kaniko" } }
 
     options {
         skipDefaultCheckout(true)
     }
 
     stages {
-        stage('Build') {
-            agent { node { label "pod-kaniko" } }
+        stage('Checkout') {
+            steps {
+                echo "> Current workspace : ${workspace}"
+                echo "> activeProfile : ${activeProfile}"
+                echo "> dockerRegistry : ${dockerRegistry}"
+                echo "> registryCredential : ${registryCredential}"
+                echo "> dockerImageName : ${dockerImageName}"
 
-            stages {
-                stage('Checkout') {
-                    steps {
-                        echo "> Current workspace : ${workspace}"
-                        echo "> activeProfile : ${activeProfile}"
-                        echo "> dockerRegistry : ${dockerRegistry}"
-                        echo "> registryCredential : ${registryCredential}"
-                        echo "> dockerImageName : ${dockerImageName}"
-
-                        checkout scm
-                    }
+                checkout scm
+            }
+        }
+        stage('Build Package') {
+            steps {
+                container(name: "container-maven") {
+                    sh "mvn -P ${activeProfile} -Dmaven.test.skip=true clean package"
                 }
-                stage('Build Package') {
-                    steps {
-                        container(name: "container-maven") {
-                            sh "mvn -P ${activeProfile} -Dmaven.test.skip=true clean package"
-                        }
-                    }
-                }
+            }
+        }
 
-                stage('Build Docker Image') {
-                    steps {
-                        container(name: "container-kaniko", shell: "/busybox/sh") {
-                            withCredentials([file(credentialsId: 'secret-kaniko', variable: 'CONF_KANIKO')]) {
-                                sh "mkdir -p /kaniko/.docker"
-                                sh "cp $CONF_KANIKO /kaniko/.docker/config.json"
-                                sh "/kaniko/executor -f `pwd`/Dockerfile --context=`pwd` --insecure --skip-tls-verify --cache=true --destination=$dockerRegistry/$dockerImageName:$env.BUILD_NUMBER"
-                            }
-                        }
+        stage('Build Docker Image') {
+            steps {
+                container(name: "container-kaniko", shell: "/busybox/sh") {
+                    withCredentials([file(credentialsId: 'secret-kaniko', variable: 'CONF_KANIKO')]) {
+                        sh "mkdir -p /kaniko/.docker"
+                        sh "cp $CONF_KANIKO /kaniko/.docker/config.json"
+                        sh "/kaniko/executor -f `pwd`/Dockerfile --context=`pwd` --insecure --skip-tls-verify --cache=true --destination=$dockerRegistry/$dockerImageName:$env.BUILD_NUMBER"
                     }
                 }
             }
         }
 
-        stage('Kubernetes Deploy') {
-            agent none
+        steps {
+            withKubeConfig([credentialsId: 'kube-secret']) {
+                sh "sed -i \"s,__IMAGE_NAME__,$dockerRegistry/$dockerImageName:$env.BUILD_NUMBER,\" k8s_deployment.yaml"
 
-            steps {
-                withKubeConfig([credentialsId: 'kube-secret']) {
-                    sh "sed -i \"s,__IMAGE_NAME__,$dockerRegistry/$dockerImageName:$env.BUILD_NUMBER,\" k8s_deployment.yaml"
-                    sh "/usr/bin/kubectl apply -f k8s_deployment.yaml"
-                }
+                sh 'curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"'
+                sh 'chmod u+x ./kubectl'
+                sh "./kubectl apply -f k8s_deployment.yaml"
             }
         }
     }
