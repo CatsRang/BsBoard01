e5509385953e71e8704a88aca562362262effc02
hwlee <bless2k@gmail.com>
2023-01-02 15:01:00 +0900

    jenkins pipeline config modified.

---
+++ jenkinsfile03.groovy
@@ -0,0 +1,66 @@
+properties([parameters([
+        choice(choices: ['dev', 'prod'], name: 'activeProfile', description: 'Maven Active Profile'),
+        choice(choices: ['registry.hub.docker.com', 'phis.harbor.io'], name: 'dockerRegistry', description: 'dockerRegistry'),
+        choice(choices: ['dockerhub-bless2k'], name: 'registryCredential', description: 'registryCredential'),
+        choice(choices: ['bless2k/pqm-api'], name: 'dockerImageName', description: 'dockerImageName')
+])])
+
+pipeline {
+    agent none
+
+    options {
+        skipDefaultCheckout(true)
+    }
+
+    stages {
+        stage('Checkout') {
+            steps {
+                echo "> Current workspace : ${workspace}"
+                echo "> activeProfile : ${activeProfile}"
+                echo "> dockerRegistry : ${dockerRegistry}"
+                echo "> registryCredential : ${registryCredential}"
+                echo "> dockerImageName : ${dockerImageName}"
+
+                checkout scm
+                stash includes: 'k8s_deployment.yaml', name: 'K8S_DEPL'
+            }
+        }
+
+        stage('Build Package') {
+            withMaven(maven: 'MavenM3') {
+                sh "mvn -P ${activeProfile} -Dmaven.test.skip=true clean package"
+                stash includes: 'target/target/BsBoard-*.jar', name: 'APP_JAR'
+            }
+        }
+
+        stage('Build') {
+            agent { node { label "pod-kaniko" } }
+
+            stage('Build Docker Image') {
+                steps {
+                    container(name: "container-kaniko", shell: "/busybox/sh") {
+                        withCredentials([file(credentialsId: 'secret-kaniko', variable: 'CONF_KANIKO')]) {
+                            sh "mkdir -p /kaniko/.docker"
+                            sh "cp $CONF_KANIKO /kaniko/.docker/config.json"
+                            unstash 'APP_JAR'
+                            sh "/kaniko/executor -f `pwd`/Dockerfile --context=`pwd` --insecure --skip-tls-verify --cache=true --destination=$dockerRegistry/$dockerImageName:$env.BUILD_NUMBER"
+                        }
+                    }
+                }
+            }
+        }
+
+        stage('Kubernetes Deploy') {
+            agent any
+
+            steps {
+                withKubeConfig([credentialsId: 'kube-secret']) {
+                    unstash 'K8S_DEPL'
+                    echo "NODE_NAME = ${env.NODE_NAME}"
+                    sh "sed -i \"s,__IMAGE_NAME__,${dockerImageName}:${env.BUILD_NUMBER},\" k8s_deployment.yaml"
+                    sh "/usr/bin/kubectl apply -f k8s_deployment.yaml"
+                }
+            }
+        }
+    }
+}
