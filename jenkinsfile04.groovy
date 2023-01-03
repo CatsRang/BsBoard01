86b43f743a1671b32dc73f3542420a646ecaa48c
hwlee <bless2k@gmail.com>
2023-01-04 00:00:12 +0900

    jenkins pipeline config modified.

--- jenkinsfile04.groovy
+++ jenkinsfile04.groovy
@@ -42,8 +42,8 @@ pipeline {
                     withCredentials([file(credentialsId: 'secret-kaniko', variable: 'CONF_KANIKO')]) {
                         unstash 'DOCKER_FILE'
                         unstash 'APP_JAR'
-                        sh "mkdir -p /kaniko/.docker"
-                        sh "cp $CONF_KANIKO /kaniko/.docker/config.json"
+                        //sh "mkdir -p /kaniko/.docker"
+                        //sh "cp $CONF_KANIKO /kaniko/.docker/config.json"
                         sh "/kaniko/executor -f `pwd`/Dockerfile -c `pwd` --insecure --skip-tls-verify -d ${dockerRegistry}/${dockerImageName}:${env.BUILD_NUMBER}"
                     }
                 //}
