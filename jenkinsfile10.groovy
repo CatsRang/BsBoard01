fd3959a578955b5366935cd8417556dd41cd2fff
hwlee <bless2k@gmail.com>
2023-01-04 21:45:48 +0900

    jenkins pipeline config modified.

--- jenkinsfile10.groovy
+++ jenkinsfile10.groovy
@@ -2,7 +2,7 @@ node('builder-node') {
     properties([[$class: 'ParametersDefinitionProperty', parameterDefinitions: [
             [$class: 'ChoiceParameterDefinition', description: 'Maven Active Profile', choices: ['dev', 'prd'], name: 'activeProfile'],
             [$class: 'StringParameterDefinition', defaultValue: 'registry.hub.docker.com', description: 'Registry Url. ex) phis.harbor.io  registry.hub.docker.com', name: "dockerRegistry"],
-            [$class: 'StringParameterDefinition', defaultValue: 'dockerhub-bless2k', description: 'Registry Credential', name: "registryCredential"],
+            [$class: 'StringParameterDefinition', defaultValue: 'cred-dockerhub-bless2k', description: 'Registry Credential', name: "registryCredential"],
             [$class: 'StringParameterDefinition', defaultValue: 'bless2k/pqm-apii', description: 'Docker Image Name', name: "dockerImageName"]
     ]]])
 
